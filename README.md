90f9b43474fe953d84683e0c30a0d1023dc31c1a
Alex Rempel <alexey.rempel@googlemail.com>
2025-03-05 18:43:40 +0100

    add fire

--- README.md
+++ README.md
@@ -13,27 +13,7 @@ firebase deploy
 
 ### TODO
 
-```
-npm install firebase
-```
+#### auth
 
-```
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA-Bo1pxZ6diAoLvWBlhGhRJBiYCR0l5sE",
-  authDomain: "dd-scheddoc.firebaseapp.com",
-  databaseURL: "https://dd-scheddoc-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "dd-scheddoc",
-  storageBucket: "dd-scheddoc.firebasestorage.app",
-  messagingSenderId: "303691106802",
-  appId: "1:303691106802:web:65f3062c113bc28f68c36b"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)`
-```
+- added google
+  - project name: `project-303691106802`
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import {provideHttpClient} from '@angular/common/http';
 import {LOCALE_ID, provideAppInitializer, provideExperimentalZonelessChangeDetection} from '@angular/core';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {provideRouter, withComponentInputBinding} from '@angular/router';
 import {AppComponent} from './app/app.component';
@@ -11,6 +14,20 @@ bootstrapApplication(AppComponent, {
     // ng
     provideHttpClient(),
     provideExperimentalZonelessChangeDetection(),
+    // 3rd party
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'dd-scheddoc',
+        appId: '1:303691106802:web:65f3062c113bc28f68c36b',
+        databaseURL: 'https://dd-scheddoc-default-rtdb.europe-west1.firebasedatabase.app',
+        storageBucket: 'dd-scheddoc.firebasestorage.app',
+        apiKey: 'AIzaSyA-Bo1pxZ6diAoLvWBlhGhRJBiYCR0l5sE',
+        authDomain: 'dd-scheddoc.firebaseapp.com',
+        messagingSenderId: '303691106802',
+      }),
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
     // local
     {provide: LOCALE_ID, useValue: 'de-DE'},
     provideAppInitializer(initializeLocaleFactory(['de-DE'])),
