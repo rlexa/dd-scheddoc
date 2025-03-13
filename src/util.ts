696f47b58398c694eab5ec93c80d97d8d1e3de1d
Alex Rempel <alexey.rempel@googlemail.com>
2025-03-13 12:14:49 +0100

    meh

--- src/util.ts
+++ src/util.ts
@@ -14,3 +14,13 @@ export const strPadStart = (padWith: string) => (max: number) => (val: string | 
 export const strPadStartWithZero = strPadStart('0');
 export const strPadStartWithZero2 = strPadStartWithZero(2);
 export const strPadStartWithZero4 = strPadStartWithZero(4);
+
+export function downloadBlob(blob: Blob, fileName: string) {
+  const elem = document.createElement('a');
+  elem.href = window.URL.createObjectURL(blob);
+  elem.download = fileName || 'blob.data';
+  document.body.appendChild(elem);
+  elem.click();
+  window.URL.revokeObjectURL(elem.href);
+  document.body.removeChild(elem);
+}
